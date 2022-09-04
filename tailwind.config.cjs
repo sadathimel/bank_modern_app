4d7af513e6a3155f564049b26b81fe876374bf51
sadathimel <sadat.himel@gmail.com>
2022-09-04 13:01:14 +0600

    create modern bank app with tailwind and reactJs

---
+++ tailwind.config.cjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html','./src/**/*.{html,js}'],
+  mode : "jit", 
+  theme: {
+    extend: {
+    colors: {
+      primary: "#00040f",
+      secondary: "#00f6ff",
+      dimWhite: "rgba(255, 255, 255, 0.7)",
+      dimBlue: "rgba(9, 151, 124, 0.1)",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+    borderRadius: {
+      '4xl': '2rem',
+    }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    }
+  },
+  plugins: [],
+}
