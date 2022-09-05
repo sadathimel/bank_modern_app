867286b97ef9cc92e75be470ec0f5c5753aa57c4
sadathimel <sadat.himel@gmail.com>
2022-09-05 11:57:28 +0600

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,16 @@
-import Billings from './Billings';
+import Billings from './Billing';
 import Business from './Business';
 import Button from './Button';
 import CardDeal from './CardDeal';
 import Clients from './Clients';
 import CTA from './CTA';
-import Feedback from './Feedback';
+import Feedback from './FeedbackCard';
 import Footer from './Footer';
 import GetStarted from './GetStarted';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import Stats from './Stats';
-import Testimonial from './Testimonial';
+import Testimonial from './Testimonials';
 
 export {
     Billings,
@@ -26,6 +26,4 @@ export {
     Navbar,
     Stats,
     Testimonial,
-
-
 };
